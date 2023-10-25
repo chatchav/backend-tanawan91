33b9a92704184b7ebe44a743ce9f35a3efb2e617
chatchav <chatchavan.s@pulsetech.co.th>
2023-10-26 00:14:53 +0700

    update req

--- assets/includes/nevbar.php
+++ assets/includes/nevbar.php
@@ -5,7 +5,7 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
     <a class="navbar-brand ps-3" href="">
-        <img style="width: 70%;" src="assets/img/new-logo-tanawan91.png" alt="">
+        <img style="width: 70%;" src="/assets/img/new-logo-tanawan91.png" alt="">
     </a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
--- js/nevbar.js
+++ js/nevbar.js
@@ -16,4 +16,25 @@ $(document).ready(function(){
             }
         });
     });
+
+    var url = window.location.pathname;
+    var menu = $(".nav").find(".nav-link");
+    console.log(url,menu);
+
+    $.each( menu, function( key, value ) {
+        var ele = $(value);
+        var href = ele.attr("href");
+        ele.removeClass( "active" );
+        if(href.search(url) != -1){
+            var id = ele.parent().parent().attr("id");
+            console.log(id);
+            if(id != undefined){
+                $('a[data-bs-target="#'+id+'"]').removeClass("collapsed").attr("aria-expanded","true");
+                $('#'+id).addClass("show");
+                console.log($('a[data-bs-target="#'+id+'"]'));
+            }
+            ele.addClass( "active" );
+        }
+        //console.log(ele.parent().parent().attr("id"), ele.find(".nav-link") ,href.search(url) );
+      });
 });
